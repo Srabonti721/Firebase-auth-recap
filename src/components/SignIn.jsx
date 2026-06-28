67af2e78b99a57a0360284110a909e14b3435c6f
Srabonti721 <somayaaktarshabonti721@gmail.com>
2026-06-28 14:18:44 +0600

    added github sign in and email intrface

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,4 +1,4 @@
-import { GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
+import { GithubAuthProvider, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
 import React, { useState } from 'react';
 import { auth } from '../firebase/firebase.init';
 import { resume } from 'react-dom/server';
@@ -6,6 +6,7 @@ import { resume } from 'react-dom/server';
 const SignIn = () => {
     const [user, setUser] = useState(null)
     const provider = new GoogleAuthProvider();
+    const githubProvider = new GithubAuthProvider();
     const handleSignIn = () => {
         signInWithPopup(auth, provider)
             .then(result => {
@@ -17,6 +18,27 @@ const SignIn = () => {
                 console.log(error);
 
             })
+    }
+
+    const handleGithubSignIn = () =>{
+        signInWithPopup(auth,githubProvider)
+        .then(result=>{
+              const githubSignInUser = result.user;
+              console.log(githubSignInUser);
+              
+              if(!githubSignInUser.email && githubSignInUser?.providerData?.length){
+                console.log("user email is not directly provided");
+                if(githubSignInUser.providerData[0].email){
+                    githubSignInUser.email = githubSignInUser.providerData[0].email;
+                       setUser(githubSignInUser)
+                }
+              }
+                
+        })
+        .catch(error=>{
+            console.log(error);
+            
+        })
     }
 
     const handleSignOut = () =>{
@@ -34,7 +56,10 @@ const SignIn = () => {
         <div>
             <h1>please sign in</h1>
             {
-                user ? <button onClick={handleSignOut}>Sign out</button>:<button onClick={handleSignIn}>SIgn in with google</button>
+                user ? <button onClick={handleSignOut}>Sign out</button>:
+                <><button onClick={handleSignIn}>SIgn in with google</button>
+                <button onClick={handleGithubSignIn}> Sign in with github</button>
+                </>
             }
             
            
